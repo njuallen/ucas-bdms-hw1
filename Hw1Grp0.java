8f5aa9413bcf56762928a1091e12b1be855fc73b
Allen <njuallen@foxmail.com>
2019-04-04 08:12:55 +0000

    Deal with the corner case: when there are multiple matches.

--- Hw1Grp0.java
+++ Hw1Grp0.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
 import java.io.*;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -120,12 +122,12 @@ public class Hw1Grp0 {
     putRecords(filteredRecords, resultColumns);
   }
 
-   /**
-    * Load records from HDFS
-    * @param file HDFSFileName
-    * @return records array of records
-    * @exception IOException, URISyntaxException
-    */
+  /**
+   * Load records from HDFS
+   * @param file HDFSFileName
+   * @return records array of records
+   * @exception IOException, URISyntaxException
+   */
   private static ArrayList<Record> loadHDFSRecords(String file) throws IOException, URISyntaxException{
 
     Configuration conf = new Configuration();
@@ -147,12 +149,12 @@ public class Hw1Grp0 {
     return v;
   }
 
-   /**
-    * Load records from local file system, used for debugging
-    * @param file LocalFileName
-    * @return records array of records
-    * @exception IOException
-    */
+  /**
+   * Load records from local file system, used for debugging
+   * @param file LocalFileName
+   * @return records array of records
+   * @exception IOException
+   */
   private static ArrayList<Record> loadLocalRecords(String file) throws IOException {
     FileInputStream in_stream = new FileInputStream(file);
     BufferedReader in = new BufferedReader(new InputStreamReader(in_stream));
@@ -168,14 +170,14 @@ public class Hw1Grp0 {
     return v;
   }
 
-   /**
-    * Join records with naive nested for loops
-    * @param rRecords records from table 1
-    * @param sRecords records from table 2
-    * @param rJoinColumnIndex specify which column in table 1 is used to join
-    * @param sJoinColumnIndex specify which column in table 2 is used to join
-    * @return joinedRecords array of joinedRecords
-    */
+  /**
+   * Join records with naive nested for loops
+   * @param rRecords records from table 1
+   * @param sRecords records from table 2
+   * @param rJoinColumnIndex specify which column in table 1 is used to join
+   * @param sJoinColumnIndex specify which column in table 2 is used to join
+   * @return joinedRecords array of joinedRecords
+   */
   private static ArrayList<JoinedRecord> loopJoinRecords(ArrayList<Record> rRecords,
       ArrayList<Record> sRecords, int rJoinColumnIndex, int sJoinColumnIndex) {
     ArrayList<JoinedRecord> v = new ArrayList<JoinedRecord>();
@@ -191,16 +193,16 @@ public class Hw1Grp0 {
     return v;
   }
 
-   /**
-    * Join records with hash join, first hash records into different buckets,
-    * then do loop join for records in buckets.
-    * @param bucketSize hash bucket size
-    * @param rRecords records from table 1
-    * @param sRecords records from table 2
-    * @param rJoinColumnIndex specify which column in table 1 is used to join
-    * @param sJoinColumnIndex specify which column in table 2 is used to join
-    * @return joinedRecords array of joinedRecords
-    */
+  /**
+   * Join records with hash join, first hash records into different buckets,
+   * then do loop join for records in buckets.
+   * @param bucketSize hash bucket size
+   * @param rRecords records from table 1
+   * @param sRecords records from table 2
+   * @param rJoinColumnIndex specify which column in table 1 is used to join
+   * @param sJoinColumnIndex specify which column in table 2 is used to join
+   * @return joinedRecords array of joinedRecords
+   */
   private static ArrayList<JoinedRecord> hashJoinRecords(int bucketSize, ArrayList<Record> rRecords,
       ArrayList<Record> sRecords, int rJoinColumnIndex, int sJoinColumnIndex) {
     // create hash buckets
@@ -236,12 +238,12 @@ public class Hw1Grp0 {
     return v;
   }
 
-   /**
-    * Filter records, only return needed columns, discard others
-    * @param joinedRecords joinedRecords
-    * @param resultColumns the columns that you need
-    * @return filterRecords filteredRecords
-    */
+  /**
+   * Filter records, only return needed columns, discard others
+   * @param joinedRecords joinedRecords
+   * @param resultColumns the columns that you need
+   * @return filterRecords filteredRecords
+   */
   private static ArrayList filterRecords(ArrayList<JoinedRecord> joinedRecords,
       ResultColumn[] resultColumns) {
     ArrayList<FilteredRecord> v = new ArrayList<FilteredRecord>();
@@ -263,12 +265,12 @@ public class Hw1Grp0 {
     return v;
   }
 
-   /**
-    * Store final results to hbase
-    * @param records final result
-    * @param resultColumns the columns that you need
-    * @return Nothing
-    */
+  /**
+   * Store final results to hbase
+   * @param records final result
+   * @param resultColumns the columns that you need
+   * @return Nothing
+   */
   private static void putRecords(ArrayList<FilteredRecord> records,
       ResultColumn[] resultColumns)
     throws MasterNotRunningException, ZooKeeperConnectionException, IOException {
@@ -309,6 +311,9 @@ public class Hw1Grp0 {
 
     admin.close();
 
+    // there may be multiple matches
+    Map matchCount = new TreeMap();
+
     // put filtered records to database
     HTable table = new HTable(conf, tableName);
 
@@ -318,11 +323,19 @@ public class Hw1Grp0 {
       String rowKey = record.key;
       String []values = record.record.record;
 
+      Integer count;
+      if ((count = (Integer)matchCount.get(rowKey)) == null)
+        count = new Integer(0);
+      matchCount.put(rowKey, new Integer(count.intValue() + 1));
+
       Put put = new Put(rowKey.getBytes());
+      // there may be multiple values
       for (int j = 0; j < resultColumns.length; j++) {
         boolean isRFile = resultColumns[j].isRFile;
         int columnIndex = resultColumns[j].columnIndex;
         String key = (isRFile ? "R" : "S") + String.format("%d", columnIndex);
+        if (count.intValue() != 0)
+          key += String.format(".%d", count.intValue());
         String value = values[j];
         System.out.printf("put record: rowKey: %s key: %s value: %s\n",
             rowKey, key, value);
